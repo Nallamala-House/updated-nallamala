6efd9878e1f5cc87ca61af6a7a805124f436f78e
PRODHOSH V.S <prodhosh3@gmail.com>
2026-04-03 09:56:54 +0530

    reduced opacity of beams background

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
           height: '100vh',
           zIndex: -1,
           pointerEvents: 'none',
-          opacity: 0.5
+          opacity: 0.4
         }}>
           <Beams
             beamWidth={3.3}
